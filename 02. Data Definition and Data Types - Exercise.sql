03acb22b270bd80592a574c40dcd93508c38bc0b
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-05-14 12:16:50 +0300

    Update 02. Data Definition and Data Types - Exercise.sql

--- 02. Data Definition and Data Types - Exercise.sql
+++ 02. Data Definition and Data Types - Exercise.sql
@@ -1,9 +1,7 @@
-```mysql
-
-/* 01. Create Tables - In the newly created database Minions add table minions (id, name, age). Then add new table towns (town_id,
-name). Set id and town_id columns of both tables to be primary key as constraint, id's must be auto increment.
-Submit yours create table queries in Judge together for both tables (one after another separated by ";") as Run
-queries & check DB. */
+//01. Create Tables - In the newly created database Minions add table minions (id, name, age). Then add new table towns (town_id,
+//name). Set id and town_id columns of both tables to be primary key as constraint, id's must be auto increment.
+//Submit yours create table queries in Judge together for both tables (one after another separated by ";") as Run
+//queries & check DB.
 
 CREATE TABLE minions (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -218,6 +216,3 @@ values
 
 // 17. Increase Employees Salary - Use softuni database and increase the salary of all employees by 10%. Then select only salary column from the
 // employees table. Submit your query statements as Prepare DB & Run queries.
-
-
-```
