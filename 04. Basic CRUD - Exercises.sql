a95ad41e37532dd1b25d1a75a7252a3155a0c688
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-05-15 20:32:06 +0300

    Update 04. Basic CRUD - Exercises.sql

--- 04. Basic CRUD - Exercises.sql
+++ 04. Basic CRUD - Exercises.sql
@@ -17,3 +17,13 @@ SELECT
 FROM
     departments
 ORDER BY department_id;
+
+/* 3. Find salary of Each Employee
+Write SQL query to find the first name, last name and salary of each employee. Sort the information by id. Submit
+your query statements as Prepare DB & run queries */
+
+SELECT 
+    first_name, last_name, salary
+FROM
+    employees
+ORDER BY employee_id;
