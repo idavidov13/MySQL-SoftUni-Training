13cb55bbff133d9977a6b16c1a1c01f5304a5e78
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-05-23 11:41:31 +0300

    Update 06. Built-in Functions - Excercise.sql

--- 06. Built-in Functions - Excercise.sql
+++ 06. Built-in Functions - Excercise.sql
@@ -205,14 +205,47 @@ ORDER BY `email provider` , user_name;
 -- where the ip_address matches the pattern: "___.1%.%.___". Submit your query statements as Prepare DB & run
 -- queries.
 
+SELECT 
+    user_name, ip_address
+FROM
+    users
+WHERE
+    ip_address LIKE '___.1%.%.___'
+ORDER BY user_name;
+
 -- 15. Show All Games with Duration and Part of the Day
 -- Find all games with their corresponding part of the day and duration. Parts of the day should be Morning (start
 -- time is >= 0 and < 12), Afternoon (start time is >= 12 and < 18), Evening (start time is >= 18 and < 24). Duration
 -- should be Extra Short (smaller or equal to 3), Short (between 3 and 6 including), Long (between 6 and 10 including)
 -- and Extra Long in any other cases or without duration. Submit your query statements as Prepare DB & run queries.
 
+SELECT 
+    name AS game,
+    CASE
+        WHEN HOUR(start) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN HOUR(start) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS 'Part if the day',
+    CASE
+        WHEN duration <= 3 THEN 'Extra Short'
+        WHEN duration BETWEEN 4 AND 6 THEN 'Short'
+        WHEN duration BETWEEN 7 AND 10 THEN 'Long'
+        ELSE 'Extra Long'
+    END AS 'Duration'
+FROM
+    games
+ORDER BY name;
+
 -- 16. Orders Table
 -- You are given a table orders (id, product_name, order_date) filled with data. Consider that the payment for an
 -- order must be accomplished within 3 days after the order date. Also the delivery date is up to 1 month. Write a
 -- query to show each product's name, order date, pay and deliver due dates. Submit your query statements as
 -- Prepare DB & run queries.
+
+SELECT 
+    product_name,
+    order_date,
+    DATE_ADD(order_date, INTERVAL 3 DAY) AS pay_due,
+    DATE_ADD(order_date, INTERVAL 1 MONTH) AS deliver_due
+FROM
+    orders
