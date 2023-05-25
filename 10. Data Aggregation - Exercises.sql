e2986b8e91f1797fb37b91a18af47f04bc7cbd0a
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-05-25 10:34:16 +0300

    Update 10. Data Aggregation - Exercises.sql

--- 10. Data Aggregation - Exercises.sql
+++ 10. Data Aggregation - Exercises.sql
@@ -1,27 +1,77 @@
 -- 1. Records' Count
 -- Import the database and send the total count of records to Mr. Bodrog. Make sure nothing got lost
 
+SELECT 
+    COUNT(*) AS `count`
+FROM
+    wizzard_deposits;
+    
 -- 2. Longest Magic Wand
 -- Select the size of the longest magic wand. Rename the new column appropriately
 
+SELECT 
+    MAX(magic_wand_size) AS `longest_magic_wand`
+FROM
+    wizzard_deposits;
+    
 -- 3. Longest Magic Wand Per Deposit Groups
 -- For wizards in each deposit group show the longest magic wand. Sort result by longest magic wand for each deposit
 -- group in increasing order, then by deposit_group alphabetically. Rename the new column appropriately.
 
+SELECT 
+    `deposit_group`,
+    MAX(`magic_wand_size`) AS `longest_magic_wand`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand` , `deposit_group`;
+
 -- 4. Smallest Deposit Group Per Magic Wand Size*
 -- Select the deposit group with the lowest average wand size.
 
+SELECT 
+    deposit_group
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY AVG(magic_wand_size)
+LIMIT 1;
+
 -- 5. Deposits Sum
 -- Select all deposit groups and its total deposit sum. Sort result by total_sum in increasing order.
+
+SELECT 
+    `deposit_group`, SUM(`deposit_amount`) AS 'total sum'
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total sum` ASC;
 
 -- 6. Deposits Sum for Ollivander Family
 -- Select all deposit groups and its total deposit sum but only for the wizards who has their magic wand crafted by
 -- Ollivander family. Sort result by deposit_group alphabetically
 
+SELECT 
+    `deposit_group`, SUM(`deposit_amount`) AS 'total sum'
+FROM
+    wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group ASC;
+
 -- 7. Deposits Filter
 -- Select all deposit groups and its total deposit sum but only for the wizards who has their magic wand crafted by
 -- Ollivander family. After this, filter total deposit sums lower than 150000. Order by total deposit sum in descending
 -- order.
+
+SELECT 
+    `deposit_group`, SUM(`deposit_amount`) AS 'total sum'
+FROM
+    wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING `total sum` < 150000
+ORDER BY `total sum` DESC;
 
 -- 8. Deposit Charge
 -- Create a query that selects:
@@ -30,6 +80,15 @@
 -- • Minimum deposit charge for each group
 -- Group by deposit_group and magic_wand_creator.
 -- Select the data in ascending order by magic_wand_creator and deposit_group.
+
+SELECT 
+    deposit_group,
+    magic_wand_creator,
+    MIN(deposit_charge) AS 'min_deposit_charge'
+FROM
+    wizzard_deposits
+GROUP BY deposit_group , magic_wand_creator
+ORDER BY magic_wand_creator ASC , deposit_group ASC;
 
 -- 9. Age Groups
 -- Write down a query that creates 7 different groups based on their age.
