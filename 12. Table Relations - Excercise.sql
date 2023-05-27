d02c18d9b7bf230fa84c2b00f698b50c3942f5a7
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-05-27 20:52:06 +0300

    Update 12. Table Relations - Excercise.sql

--- 12. Table Relations - Excercise.sql
+++ 12. Table Relations - Excercise.sql
@@ -1,1 +1,55 @@
+/*1. One-To-One Relationship
+Create two tables as follows. Use appropriate data types
+Insert the data from the example above.
+• Alter table people and make person_id a primary key.
+• Create a foreign key between people and passports by using the passport_id column.
+• Think about which passport field should be UNIQUE.
+• Format salary to second digit after decimal point.
+Submit your queries by using "MySQL run queries & check DB" strategy.*/
+
+/*2. One-To-Many Relationship
+Create two tables as follows. Use appropriate data types.
+Insert the data from the example above.
+• Add primary and foreign keys.
+Submit your queries by using "MySQL run queries & check DB" strategy  */
+
+/*3. Many-To-Many Relationship
+Create three tables as follows. Use appropriate data types.
+Insert the data from the example above.
+• Add primary and foreign keys.
+• Have in mind that the table student_exams should have a
+composite primary key.
+Submit your queries by using "MySQL run queries & check DB" strategy.*/
+
+/*4. Self-Referencing
+Create a single table as follows. Use appropriate data types.
+Insert the data from the example above.
+• Add primary and foreign keys.
+• The foreign key should be between manager_id and teacher_id.
+Submit your queries by using " MySQL run queries & check DB" strategy.*/
+
+/*5. Online Store Database
+Create a new database and design the following structure:
+Submit your queries by using "MySQL run queries & check DB" strategy.*/
+
+/*6. University Database
+Create a new database and design the following structure:
+Submit your queries by using "MySQL run queries & check DB" strategy.*/
+
+/*7. SoftUni Design
+Create an E/R Diagram of the SoftUni Database. There are some special relations you should check out: employees
+are self-referenced (manager_id) and departments have One-to-One with the employees (manager_id) while the
+employees have One-to-Many (department_id). You might find it interesting how it looks on а diagram.*/
+
+/*8. Geography Design
+Create an E/R Diagram of the Geography Database.*/
+
+/*9. Peaks in Rila
+Display all peaks for "Rila" mountain_range. Include:
+• mountain_range
+© SoftUni – about.softuni.bg. Copyrighted document. Unauthorized copy, reproduction or use is not permitted.
+ Follow us: Page 5 of 5
+• peak_name
+• peak_elevation
+Peaks should be sorted by peak_elevation descending.*/
 
