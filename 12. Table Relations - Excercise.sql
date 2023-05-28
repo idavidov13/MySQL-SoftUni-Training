f1db0a7506b31a549d65fd1252efcfddb965df4a
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-05-28 15:41:45 +0300

    Update 12. Table Relations - Excercise.sql

--- 12. Table Relations - Excercise.sql
+++ 12. Table Relations - Excercise.sql
@@ -128,6 +128,40 @@ Submit your queries by using "MySQL run queries & check DB" strategy.*/
 Create a new database and design the following structure:
 Submit your queries by using "MySQL run queries & check DB" strategy.*/
 
+CREATE TABLE subjects (
+    subject_id INT PRIMARY KEY AUTO_INCREMENT,
+    subject_name VARCHAR(50)
+);
+CREATE TABLE majors (
+    major_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name VARCHAR(50),
+    student_number VARCHAR(12),
+	major_id INT,
+    CONSTRAINT fk_students_majors FOREIGN KEY (major_id)
+        REFERENCES majors (major_id)
+);
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+    payment_date DATE,
+    payment_amount DECIMAL(8 , 2 ),
+    student_id INT,
+    CONSTRAINT fk_payments_students FOREIGN KEY (student_id)
+        REFERENCES students (student_id)
+);
+CREATE TABLE agenda (
+    student_id INT,
+    subject_id INT,
+    CONSTRAINT pk_agenda PRIMARY KEY (student_id , subject_id),
+    CONSTRAINT fk_agenda_students FOREIGN KEY (student_id)
+        REFERENCES students (student_id),
+    CONSTRAINT fk_agenda_subjects FOREIGN KEY (subject_id)
+        REFERENCES subjects (subject_id)
+);
+
 /*7. SoftUni Design
 Create an E/R Diagram of the SoftUni Database. There are some special relations you should check out: employees
 are self-referenced (manager_id) and departments have One-to-One with the employees (manager_id) while the
